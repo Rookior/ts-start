00317d0b589026d957015ddd23ed4c4604868b3f
Rookior <1757629766@qq.com>
2021-08-30 09:41:10 +0800

    生产与开发环境webpack配置

---
+++ webpack.config.pro.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry:"./src/main.ts",
+    output: {
+        path:path.resolve(__dirname,'dist'),
+        filename:'main.js',
+        assetModuleFilename: 'static/[hash][ext][query]'     
+    },
+    plugins: [
+        // 清空dist文件夹后再生成bundle
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({         
+            template: './src/index.html',  //读取模板的入口文件
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    devServer: {
+        contentBase: "./dist",
+        open: true,
+    },
+    resolve: {
+        // 路径别名
+        extensions: ['.ts','.js', '.vue', '.json'],
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[MiniCssExtractPlugin.loader,'css-loader'],
+                exclude:[
+                    path.resolve(__dirname,'src/components')
+                ]
+            },
+            {
+                test:/\.css$/,
+                use:[MiniCssExtractPlugin.loader,{
+                    loader:'css-loader',
+                    options:{
+                        modules:{
+                            localIdentName: "[path][name]__[local]--[hash:base64:5]",
+                        }
+                    }
+                }],
+                include:[
+                    path.resolve(__dirname,'src/components')
+                ]
+            },
+            // {             
+            //     test: /\.(woff|woff2|eot|ttf|otf|svg|png|svg|jpg|gif)$/,
+            //     dependency: { not: ['url'] },
+            //     use: ['file-loader'],
+            //     type: 'javascript/auto'                        
+            // },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg|png|svg|jpg|gif)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.ts$/,
+                use: ['ts-loader'],
+                exclude : /node_moudles/
+            },
+            { test: /\.vue$/, use: 'vue-loader' }
+        ]
+    },
+    mode:"production"
+
+}
